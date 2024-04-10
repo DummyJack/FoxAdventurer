925ff6b97ae89a26a808289b2ec949515545483c
Uoiqetuo <bobchen773@gmail.com>
2024-04-10 11:57:14 +0800

    wasd movement

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public vThirdPersonCamera _vThirdPersonCamera;
+    public float zoomSpeed = 5;
+    private readonly float MAX_DISTANCE = 9;
+    private readonly float MIN_DISTANCE = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        // print(scroll);
+
+        if (scroll > 0) // 滾輪向上
+        {
+            _vThirdPersonCamera.defaultDistance += 1;
+            
+
+        }
+        else if (scroll < 0) // 滾輪向下
+        {
+            _vThirdPersonCamera.defaultDistance -= 1;
+
+        }
+        _vThirdPersonCamera.defaultDistance = Mathf.Clamp(_vThirdPersonCamera.defaultDistance, MIN_DISTANCE, MAX_DISTANCE);
+    }
+}
